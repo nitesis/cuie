f3a606d5a6405e9810c494dca39021f1fc045fed
nitesis <viviane.bendjus@students.fhnw.ch>
2016-10-09 23:58:37 +0200

    added arc

--- src/main/java/ch/fhnw/cuie/module02/gradients/SlimValueDisplayPane.java
+++ src/main/java/ch/fhnw/cuie/module02/gradients/SlimValueDisplayPane.java
@@ -1,17 +1,23 @@
 package ch.fhnw.cuie.module02.gradients;
 
+import javafx.application.Application;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Shape;
 
 /**
  * @author Dieter Holz
  */
-public class SlimValueDisplayPane extends VBox {
-
+public class SlimValueDisplayPane extends StackPane{
+    private StackPane pane;
 	private Circle circle;
+    private Arc arc;
 
 	public SlimValueDisplayPane() {
         initializeSelf();
@@ -29,13 +35,17 @@ public class SlimValueDisplayPane extends VBox {
 
 
 	private void initializeParts() {
+        pane = new StackPane();
         circle = new Circle(100.0f);
-        circle.setAccessibleText("1234");
-		circle.getStyleClass().addAll("playground", "slimValueDisplayPlayground");
+        circle.setCenterX(125.0f);
+        circle.setCenterY(150.0f);
+		circle.getStyleClass().addAll("playground", "slimValueDisplayCircle");
+
+        arc = new Arc(125.0f, 150.0f, 100.0f, 100.0f, 90.0f, -314.1f);
+        arc.getStyleClass().addAll("playground", "slimValueDisplayArc");
 	}
 
 	private void layoutParts() {
-		setVgrow(circle, Priority.ALWAYS);
-		getChildren().addAll(circle);
+		getChildren().addAll(circle, arc);
 	}
 }
