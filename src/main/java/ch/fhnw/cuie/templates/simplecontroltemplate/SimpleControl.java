b1a311ba72671cf8557dae8c2b649f3c2723685c
nitesis <viviane.bendjus@students.fhnw.ch>
2016-11-16 13:32:07 +0100

    init template

---
+++ src/main/java/ch/fhnw/cuie/templates/simplecontroltemplate/SimpleControl.java
@@ -0,0 +1,326 @@
+package ch.fhnw.cuie.templates.simplecontroltemplate;
+
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.css.CssMetaData;
+import javafx.css.SimpleStyleableObjectProperty;
+import javafx.css.Styleable;
+import javafx.css.StyleableObjectProperty;
+import javafx.css.StyleablePropertyFactory;
+import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextBoundsType;
+import javafx.util.Duration;
+
+/**
+ * @author Dieter Holz
+ */
+public class SimpleControl extends Region {
+    // needed for StyleableProperties
+    private static final StyleablePropertyFactory<SimpleControl> FACTORY = new StyleablePropertyFactory<>(Region.getClassCssMetaData());
+
+    @Override
+    public List<CssMetaData<? extends Styleable, ?>> getCssMetaData() {
+        return FACTORY.getCssMetaData();
+    }
+
+    private static final String FONTS_CSS = "fonts.css";
+    private static final String STYLE_CSS = "style.css";
+
+    private static final double ARTBOARD_WIDTH  = 100;
+    private static final double ARTBOARD_HEIGHT = 100;
+
+    private static final double ASPECT_RATIO = ARTBOARD_WIDTH / ARTBOARD_HEIGHT;
+
+    private static final double MINIMUM_WIDTH  = 25;
+    private static final double MINIMUM_HEIGHT = MINIMUM_WIDTH / ASPECT_RATIO;
+
+    private static final double MAXIMUM_WIDTH = 800;
+
+    // all parts
+    private Text display;
+
+    // all properties
+    private final StringProperty value = new SimpleStringProperty();
+
+    private final BooleanProperty          timerIsRunning = new SimpleBooleanProperty(false);
+    private final ObjectProperty<Duration> pulse          = new SimpleObjectProperty<>(Duration.seconds(1.0));
+
+    //CSS stylable properties
+    private static final CssMetaData<SimpleControl, Color> BASE_COLOR_META_DATA = FACTORY.createColorCssMetaData("-base-color", s -> s.baseColor);
+
+    private final StyleableObjectProperty<Color> baseColor = new SimpleStyleableObjectProperty<Color>(BASE_COLOR_META_DATA, this, "baseColor") {
+        @Override
+        protected void invalidated() {
+            setStyle(BASE_COLOR_META_DATA.getProperty() + ": " + (getBaseColor()).toString().replace("0x", "#") + ";");
+            applyCss();
+        }
+    };
+
+    // all animations
+
+    // all parts need to be children of the drawingPane
+    private Pane drawingPane;
+
+    private final AnimationTimer timer = new AnimationTimer() {
+        private long lastTimerCall;
+
+        @Override
+        public void handle(long now) {
+            if (now > lastTimerCall + (getPulse().toMillis() * 1_000_000L)) {
+                performPeriodicTask();
+                lastTimerCall = now;
+            }
+        }
+    };
+
+    public SimpleControl() {
+        initializeSelf();
+        initializeParts();
+        layoutParts();
+        initializeAnimations();
+        addEventHandlers();
+        addValueChangedListeners();
+        setupBindings();
+    }
+
+    private void initializeSelf() {
+        addStyleSheets(this);
+        getStyleClass().add(getStyleClassName());
+    }
+
+
+    private void initializeParts() {
+        display = createCenteredText("display");
+
+        // always needed
+        drawingPane = new Pane();
+        drawingPane.setMaxSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
+        drawingPane.setMinSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
+        drawingPane.setPrefSize(ARTBOARD_WIDTH, ARTBOARD_HEIGHT);
+        drawingPane.getStyleClass().add("drawingPane");
+    }
+
+    private void layoutParts() {
+        // add all your parts here
+        drawingPane.getChildren().addAll(display);
+
+        getChildren().add(drawingPane);
+    }
+
+    private void initializeAnimations() {
+
+    }
+
+    private void addEventHandlers() {
+
+    }
+
+    private void addValueChangedListeners() {
+
+
+        // if you need the timer
+        timerIsRunning.addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                timer.start();
+            } else {
+                timer.stop();
+            }
+        });
+
+        // always needed
+        widthProperty().addListener((observable, oldValue, newValue) -> resize());
+        heightProperty().addListener((observable, oldValue, newValue) -> resize());
+    }
+
+    private void setupBindings() {
+        display.textProperty().bind(valueProperty());
+    }
+
+    private void performPeriodicTask() {
+    }
+
+
+    // some useful helper-methods
+
+    private Text createCenteredText(String styleClass) {
+        return createCenteredText(ARTBOARD_WIDTH * 0.5, ARTBOARD_HEIGHT * 0.5, styleClass);
+    }
+
+    private Text createCenteredText(double cx, double cy, String styleClass) {
+        Text text = new Text();
+        text.getStyleClass().add(styleClass);
+        text.setTextOrigin(VPos.CENTER);
+        text.setTextAlignment(TextAlignment.CENTER);
+        double width = cx > ARTBOARD_WIDTH * 0.5 ? ((ARTBOARD_WIDTH - cx) * 2.0) : cx * 2.0;
+        text.setWrappingWidth(width);
+        text.setBoundsType(TextBoundsType.VISUAL);
+        text.setY(cy);
+
+        return text;
+    }
+
+    /*
+     * angle in degrees, 0 is north
+     */
+    private double angle(double cx, double cy, double x, double y) {
+        double deltaX = x - cx;
+        double deltaY = y - cy;
+        double radius = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+        double nx     = deltaX / radius;
+        double ny     = deltaY / radius;
+        double theta  = Math.toRadians(90) + Math.atan2(ny, nx);
+
+        return Double.compare(theta, 0.0) >= 0 ? Math.toDegrees(theta) : Math.toDegrees((theta)) + 360.0;
+    }
+
+    /*
+     * angle in degrees, 0 is north
+     */
+    private Point2D pointOnCircle(double cX, double cY, double radius, double angle) {
+        return new Point2D(cX - (radius * Math.sin(Math.toRadians(angle - 180))),
+                           cY + (radius * Math.cos(Math.toRadians(angle - 180))));
+    }
+
+    /*
+     * Needed if you want to know what's defined in css during initialization
+     */
+    private void applyCss(Node node) {
+        Group group = new Group(node);
+        group.getStyleClass().add(getStyleClassName());
+        addStyleSheets(group);
+        new Scene(group);
+        node.applyCss();
+    }
+
+    private void resize() {
+        Insets padding         = getPadding();
+        double availableWidth  = getWidth() - padding.getLeft() - padding.getRight();
+        double availableHeight = getHeight() - padding.getTop() - padding.getBottom();
+
+        double width = Math.max(Math.min(Math.min(availableWidth, availableHeight * ASPECT_RATIO), MAXIMUM_WIDTH), MINIMUM_WIDTH);
+
+        double scalingFactor = width / ARTBOARD_WIDTH;
+
+        if (availableWidth > 0 && availableHeight > 0) {
+            drawingPane.relocate((getWidth() - ARTBOARD_WIDTH) * 0.5, (getHeight() - ARTBOARD_HEIGHT) * 0.5);
+            drawingPane.setScaleX(scalingFactor);
+            drawingPane.setScaleY(scalingFactor);
+        }
+    }
+
+    private void addStyleSheets(Parent parent) {
+        String fonts = getClass().getResource(FONTS_CSS).toExternalForm();
+        parent.getStylesheets().add(fonts);
+
+        String stylesheet = getClass().getResource(STYLE_CSS).toExternalForm();
+        parent.getStylesheets().add(stylesheet);
+    }
+
+    private String getStyleClassName() {
+        String className = this.getClass().getSimpleName();
+
+        return className.substring(0, 1).toLowerCase() + className.substring(1);
+    }
+
+    // compute sizes
+
+    @Override
+    protected double computeMinWidth(double height) {
+        Insets padding           = getPadding();
+        double horizontalPadding = padding.getLeft() + padding.getRight();
+
+        return MINIMUM_WIDTH + horizontalPadding;
+    }
+
+    @Override
+    protected double computeMinHeight(double width) {
+        Insets padding         = getPadding();
+        double verticalPadding = padding.getTop() + padding.getBottom();
+
+        return MINIMUM_HEIGHT + verticalPadding;
+    }
+
+    @Override
+    protected double computePrefWidth(double height) {
+        Insets padding           = getPadding();
+        double horizontalPadding = padding.getLeft() + padding.getRight();
+
+        return ARTBOARD_WIDTH + horizontalPadding;
+    }
+
+    @Override
+    protected double computePrefHeight(double width) {
+        Insets padding         = getPadding();
+        double verticalPadding = padding.getTop() + padding.getBottom();
+
+        return ARTBOARD_HEIGHT + verticalPadding;
+    }
+
+    // getter and setter for all properties
+
+    public String getValue() {
+        return value.get();
+    }
+
+    public StringProperty valueProperty() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value.set(value);
+    }
+
+    public Color getBaseColor() {
+        return baseColor.get();
+    }
+
+    public StyleableObjectProperty<Color> baseColorProperty() {
+        return baseColor;
+    }
+
+    public void setBaseColor(Color baseColor) {
+        this.baseColor.set(baseColor);
+    }
+
+    public boolean isTimerIsRunning() {
+        return timerIsRunning.get();
+    }
+
+    public BooleanProperty timerIsRunningProperty() {
+        return timerIsRunning;
+    }
+
+    public void setTimerIsRunning(boolean timerIsRunning) {
+        this.timerIsRunning.set(timerIsRunning);
+    }
+
+    public Duration getPulse() {
+        return pulse.get();
+    }
+
+    public ObjectProperty<Duration> pulseProperty() {
+        return pulse;
+    }
+
+    public void setPulse(Duration pulse) {
+        this.pulse.set(pulse);
+    }
+}
---
+++ src/main/java/ch/fhnw/cuie/templates/simplecontroltemplate/demo/DemoPane.java
@@ -0,0 +1,68 @@
+package ch.fhnw.cuie.templates.simplecontroltemplate.demo;
+
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import simplecontroltemplate.SimpleControl;
+
+/**
+ * @author Dieter Holz
+ */
+public class DemoPane extends BorderPane {
+    private SimpleControl customControl;
+
+    private TextField   valueField;
+    private CheckBox    timerRunningBox;
+    private Slider      pulseSlider;
+    private ColorPicker colorPicker;
+
+    public DemoPane() {
+        initializeControls();
+        layoutControls();
+        addBindings();
+    }
+
+    private void initializeControls() {
+        setPadding(new Insets(10));
+
+        customControl = new SimpleControl();
+
+        valueField = new TextField();
+        valueField.setText("Wow!");
+
+        timerRunningBox = new CheckBox("Timer running");
+        timerRunningBox.setSelected(false);
+
+        pulseSlider = new Slider(0.5, 2.0, 1.0);
+        pulseSlider.setShowTickLabels(true);
+        pulseSlider.setShowTickMarks(true);
+
+        colorPicker = new ColorPicker();
+    }
+
+    private void layoutControls() {
+        setCenter(customControl);
+        VBox box = new VBox(10, new Label("Control Properties"), valueField, timerRunningBox, pulseSlider, colorPicker);
+        box.setPadding(new Insets(10));
+        box.setSpacing(10);
+        setRight(box);
+    }
+
+    private void addBindings() {
+        customControl.valueProperty().bindBidirectional(valueField.textProperty());
+
+        customControl.timerIsRunningProperty().bindBidirectional(timerRunningBox.selectedProperty());
+        customControl.pulseProperty().bind(Bindings.createObjectBinding(() -> Duration.seconds(pulseSlider.getValue()), pulseSlider.valueProperty()));
+
+        colorPicker.valueProperty().bindBidirectional(customControl.baseColorProperty());
+    }
+
+}
